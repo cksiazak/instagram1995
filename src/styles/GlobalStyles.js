e7f1c0f91a4253486f4560c5e036aa57f552c893
Christopher Ksiazak <cksiazak@gmail.com>
2020-01-31 20:40:18 -0700

    restarting build to text nextjs on front end and Hapi.js on backend

--- src/styles/GlobalStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const SectionWrapper = styled.section`
-  display: flex;
-  width: 60%;
-  justify-content: center;
-  flex-direction: column;
-  padding-bottom: 100px;
-`;
